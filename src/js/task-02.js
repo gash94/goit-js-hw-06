889edd4b3fa6e1d951fb43eba35559d5daf9aabb
gash94 <gazik94@gmail.com>
2022-09-15 21:04:09 +0200

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const listIng = document.querySelector('#ingredients')
+const createItem = ingredients.map(itemArr => {
+  const listItem = document.createElement("li");
+  listItem.classList.add('item');
+  listItem.textContent = itemArr;
+  
+  return listItem
+});
+listIng.append(...createItem)
+document.Get
