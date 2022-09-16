7f1d38baf12a040e0764981a8219c693e68cb3b5
gash94 <gazik94@gmail.com>
2022-09-16 18:25:20 +0200

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyColor = document.querySelector("body");
+const randomBtn = bodyColor.querySelector(".change-color")
+const color = bodyColor.querySelector(".color")
+
+const changeColor = () => {
+  const randomColor = getRandomHexColor();
+  bodyColor.style.backgroundColor = `${randomColor}`;
+  color.textContent = `${randomColor}`;
+};
+
+randomBtn.addEventListener("click", changeColor);
