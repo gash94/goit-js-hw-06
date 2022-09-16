8cd140193f408fec698bddfb13a522db9b2244ed
gash94 <gazik94@gmail.com>
2022-09-16 13:16:31 +0200

    Update task-04.js

---
+++ src/js/task-04.js
@@ -0,0 +1,15 @@
+let counterValue = 0;
+const decrementBtn = document.querySelector('button[data-action="decrement"]');
+const incrementBtn = document.querySelector('button[data-action="increment"]');
+const value = document.getElementById('value');
+
+const decrement = () => {
+    counterValue -= 1;
+    value.textContent = counterValue;
+}
+const increment = () => {
+    counterValue += 1;
+    value.textContent = counterValue;
+}
+decrementBtn.addEventListener("click", decrement);
+incrementBtn.addEventListener("click", increment);
