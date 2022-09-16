4af9f456de0cf56c560c2093183c174e3415240f
gash94 <gazik94@gmail.com>
2022-09-16 17:31:10 +0200

    Update task-06.js

---
+++ src/js/task-06.js
@@ -0,0 +1,19 @@
+const input = document.getElementById("validation-input");
+const valueLength = input.dataset.length;
+
+const validClass = (add, remove) => {
+    input.classList.add(add);
+    input.classList.remove(remove);
+};
+
+input.addEventListener("blur", (event) => {
+    const length = event.currentTarget.value.length;
+    if (length < valueLength) {
+      return validClass("invalid", "valid")  
+    }
+    if (length >= valueLength) {
+        return validClass("valid", "invalid")  
+      }
+});
+
+
