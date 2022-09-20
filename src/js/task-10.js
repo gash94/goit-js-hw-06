cb0b9069577ff1bc6a8303632e422c53b4b24e15
gash94 <gazik94@gmail.com>
2022-09-20 20:45:03 +0200

    Update task-10.js

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,37 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+const blockControls = document.getElementById("controls");
+const input = blockControls.querySelector("input");
+const btnCreate = blockControls.querySelector("[data-create]");
+const btnDestroy = blockControls.querySelector("[data-destroy]");
+const boxes = document.querySelector("#boxes");
+
+let boxesCollection = [];
+let sizesOfBox = 30;
+
+function createBoxes(amount) {
+  console.log(amount);
+  for (var i = 0; i < amount; i += 1) {
+    let createEl = document.createElement("div");
+    createEl.style.width = `${sizesOfBox}px`;
+    createEl.style.height = `${sizesOfBox}px`;
+    sizesOfBox += 10;
+    createEl.style.backgroundColor = getRandomHexColor();
+    boxesCollection.push(createEl);
+  }
 }
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function pushBoxesCollection() {
+  createBoxes(Number(input.value));
+  boxes.append(...boxesCollection);
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = '';
+}
+
+btnCreate.addEventListener("click", pushBoxesCollection);
+btnDestroy.addEventListener("click", destroyBoxes);
