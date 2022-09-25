aaa6a507b5129c6511ebd7786839cdceb45ba442
gash94 <gazik94@gmail.com>
2022-09-25 14:45:18 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -5,13 +5,14 @@ form.addEventListener("submit", handleSubmit);
 function handleSubmit(event) {
   event.preventDefault();
   const {
-    elements: { email, password }
+    elements: { email, password },
   } = event.currentTarget;
 
   if (email.value === "" || password.value === "") {
     return alert("Proszę uzupełnić wszystkie pola!");
+  } else {
+    const result = { email: email.value, password: password.value };
+    console.log(result);
+    event.currentTarget.reset();
   }
-
-  console.log(`Email: ${email.value}, Password: ${password.value}`);
-  event.currentTarget.reset();
-};
+}
